b94659e68ed863dc68ea67059c7e8a182255b32b
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2024-10-28 23:47:16 +0300

    update build

---
+++ app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id ("java")
+    application
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
+
+
