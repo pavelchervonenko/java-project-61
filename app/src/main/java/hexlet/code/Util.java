c947d4a0c050e7fa4790b1e657dff1cb6b82b1ca
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2025-03-20 22:51:37 +0300

    new class for greeting and randomized

--- app/src/main/java/hexlet/code/Util.java
+++ app/src/main/java/hexlet/code/Util.java
@@ -1,4 +1,25 @@
 package hexlet.code;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Util {
+    static String userName;
+
+    public static void greetingUser() {
+        Scanner scanner = new Scanner((System.in));
+
+        System.out.println();
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+
+        userName = scanner.next();
+
+        System.out.println(("Hello, " + userName + "!"));
+    }
+
+    public static int randomWithBoard(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min + 1) + min;
+    }
 }
