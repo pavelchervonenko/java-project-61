85c08514e2dc97b13e2fac9c2b33c85e0824966e
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2025-03-20 22:50:51 +0300

    new logic

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,223 +1,37 @@
 package hexlet.code;
 
-import hexlet.code.games.Even;
-import hexlet.code.games.Prime;
-import hexlet.code.games.Calc;
-import hexlet.code.games.Progression;
-import hexlet.code.games.Gcd;
-
 import java.util.Scanner;
 
+// Формат массива data -> [вопрос1, вопрос2, вопрос3, ответ1, ответ2, ответ3]
+// То есть, ответ1 соответсвует правильному ответу на вопрос1
+// int i = 0;
+// data[i] = вопрос1, а data[i + 3] = ответ на вопрос1, и так далее...
+// Прибавляем всегда 3, так как три раунда и на каждый уникальный набор данных
+
 public class Engine {
-    static final int ROUNDS = 3;
-
-    public static void selectGreet() {
-        UserInteraction.greetingUser();
-    }
-
-    public static void selectEven() {
-
-        UserInteraction.greetingUser();
-
-        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-
+    public static void check(String[] data) {
         int countCorrectAnswer = 0;
+        int rounds = 3;
 
-        for (int i = 0; i < ROUNDS; i++) {
-            int currentNumber = Even.generationRandomNumber();
-            String correctAnswer = currentNumber % 2 == 0 ? "yes" : "no";
-
-            System.out.println("Question: " + currentNumber);
+        for (int i = 0; i < rounds; i++) {
+            System.out.println("Question: " + data[i]);
             System.out.print("Your answer: ");
 
             Scanner scannerUserAnswer = new Scanner(System.in);
             String userAnswer = scannerUserAnswer.next();
 
-            if (userAnswer.equals(correctAnswer)) {
+            if (userAnswer.equals(data[i + 3])) {
                 System.out.println("Correct");
                 countCorrectAnswer += 1;
-                if (countCorrectAnswer == ROUNDS) {
-                    UserInteraction.congratulationsUser();
-                }
-            } else {
-                System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.");
 
-                UserInteraction.wrongAnswerUser();
-                System.exit(0);
-            }
-        }
-    }
-
-    public static void selectCalc() {
-
-        UserInteraction.greetingUser();
-
-        System.out.println("What is the result of the expression?");
-
-        int countCorrectAnswer = 0;
-
-        for (int i = 0; i < ROUNDS; i++) {
-            int[] dataNumbersCalc = Calc.returnDataNumbersCalc();
-            String currentOperation = Calc.returnDataOperationCalc();
-
-            int currentNumberOne = dataNumbersCalc[0];
-            int currentNumberTwo = dataNumbersCalc[1];
-
-            String correctAnswer = "";
-            switch (currentOperation) {
-                case "-":
-                    correctAnswer = String.valueOf(currentNumberOne - currentNumberTwo);
-                    break;
-                case "+":
-                    correctAnswer = String.valueOf(currentNumberOne + currentNumberTwo);
-                    break;
-                case "*":
-                    correctAnswer = String.valueOf(currentNumberOne * currentNumberTwo);
-                    break;
-                default:
-                    System.out.println("Error: unknown operation");
-            }
-
-            System.out.println("Question: " + currentNumberOne + " " + currentOperation + " " + currentNumberTwo);
-            System.out.print("Your answer: ");
-
-            Scanner scannerUserAnswer = new Scanner(System.in);
-            String userAnswer = scannerUserAnswer.next();
-
-            if (userAnswer.equals(correctAnswer)) {
-                System.out.println("Correct!");
-                countCorrectAnswer += 1;
-                if (countCorrectAnswer == ROUNDS) {
-                    UserInteraction.congratulationsUser();
+                if (countCorrectAnswer == rounds) {
+                    System.out.println("Congratulations, " + Util.userName + "!");
                 }
             } else {
                 System.out.print("'" + userAnswer + "' " + "is wrong answer ;(.");
-                System.out.println(" Correct answer was " + "'" + correctAnswer + "'.");
+                System.out.println(" Correct answer was " + "'" + data[i + 3] + "'.");
 
-                UserInteraction.wrongAnswerUser();
-                System.exit(0);
-            }
-        }
-    }
-
-    public static void selectGcd() {
-
-        UserInteraction.greetingUser();
-
-        System.out.println("Find the greatest common divisor of given numbers.");
-
-        int countCorrectAnswer = 0;
-
-        for (int i = 0; i < ROUNDS; i++) {
-            long[] dataNumbersGcd = Gcd.returnDataNumbersGcd();
-
-            long currentNumberOne = dataNumbersGcd[0];
-            long currentNumberTwo = dataNumbersGcd[1];
-
-            long correctAnswer = Gcd.gcd(currentNumberOne, currentNumberTwo);
-
-            System.out.println("Question: " + currentNumberOne + " " + currentNumberTwo);
-            System.out.print("Your answer: ");
-
-            Scanner scannerUserAnswer = new Scanner(System.in);
-            if (!scannerUserAnswer.hasNextLong()) {
-                var currentScannerUserAnswer = scannerUserAnswer.next();
-
-                System.out.print("'" + currentScannerUserAnswer + "' " + "is wrong answer ;(.");
-                System.out.println(" Correct answer was " + "'" + correctAnswer + "'.");
-
-                UserInteraction.wrongAnswerUser();
-                System.exit(0);
-            }
-
-            long userAnswer = scannerUserAnswer.nextInt();
-
-            if (userAnswer == correctAnswer) {
-                System.out.println("Correct!");
-                countCorrectAnswer += 1;
-                if (countCorrectAnswer == ROUNDS) {
-                    UserInteraction.congratulationsUser();
-                }
-            } else {
-                System.out.print("'" + userAnswer + "' " + "is wrong answer ;(.");
-                System.out.println(" Correct answer was " + "'" + correctAnswer + "'.");
-
-                UserInteraction.wrongAnswerUser();
-                System.exit(0);
-            }
-        }
-    }
-    public static void selectProgression() {
-
-        UserInteraction.greetingUser();
-
-        System.out.println("What number is missing in the progression?");
-
-        int countCorrectAnswer = 0;
-
-        for (int i = 0; i < ROUNDS; i++) {
-            System.out.println("Question: " + Progression.finalProgressionInString());
-
-            int correctAnswer = Progression.returnCorrectAnswer();
-
-            System.out.print("Your answer: ");
-
-            Scanner scannerUserAnswer = new Scanner(System.in);
-            if (!scannerUserAnswer.hasNextLong()) {
-                var currentScannerUserAnswer = scannerUserAnswer.next();
-
-                System.out.print("'" + currentScannerUserAnswer + "' " + "is wrong answer ;(.");
-                System.out.println(" Correct answer was " + "'" + correctAnswer + "'.");
-
-                UserInteraction.wrongAnswerUser();
-                System.exit(0);
-            }
-
-            int userAnswer = scannerUserAnswer.nextInt();
-
-            if (userAnswer == correctAnswer) {
-                System.out.println("Correct!");
-                countCorrectAnswer += 1;
-                if (countCorrectAnswer == ROUNDS) {
-                    UserInteraction.congratulationsUser();
-                }
-            } else {
-                System.out.print("'" + userAnswer + "' " + "is wrong answer ;(.");
-                System.out.println(" Correct answer was " + "'" + correctAnswer + "'.");
-
-                UserInteraction.wrongAnswerUser();
-                System.exit(0);
-            }
-        }
-    }
-
-    public static void selectPrime() {
-        UserInteraction.greetingUser();
-
-        System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
-
-        int countCorrectAnswer = 0;
-
-        for (int i = 0; i < ROUNDS; i++) {
-            int number = Prime.returnRandomNumber();
-            String correctAnswer = Prime.returnCorrectAnswer(number);
-            System.out.println("Question: " + number);
-            System.out.print("Your answer: ");
-
-            Scanner scannerUserAnswer = new Scanner(System.in);
-            String userAnswer = scannerUserAnswer.next();
-
-            if (userAnswer.equals(correctAnswer)) {
-                System.out.println("Correct!");
-                countCorrectAnswer += 1;
-                if (countCorrectAnswer == ROUNDS) {
-                    UserInteraction.congratulationsUser();
-                }
-            } else {
-                System.out.print("'" + userAnswer + "' " + "is wrong answer ;(.");
-                System.out.println(" Correct answer was " + "'" + correctAnswer + "'.");
-
-                UserInteraction.wrongAnswerUser();
+                System.out.println("Let's try again, " + Util.userName + "!");
                 System.exit(0);
             }
         }
