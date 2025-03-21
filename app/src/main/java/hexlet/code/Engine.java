adf01c088592a5c7263ae6eb28bd363e5a88b9e0
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2025-03-21 13:52:39 +0300

    add greeting and rules for every game

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,40 +1,88 @@
 package hexlet.code;
 
+import hexlet.code.games.*;
+
 import java.util.Scanner;
 
-// Формат массива data -> [вопрос1, вопрос2, вопрос3, ответ1, ответ2, ответ3]
-// То есть, ответ1 соответсвует правильному ответу на вопрос1
+// Формат двумерного массива data -> [[вопрос1, ответ1], [вопрос2, ответ2], [вопрос3, ответ3]]
 // int i = 0;
-// data[i] = вопрос1, а data[i + 3] = ответ на вопрос1, и так далее...
-// Прибавляем всегда 3, так как три раунда и на каждый уникальный набор данных
+// data[i][0] = вопрос1, а data[i][1] = ответ на вопрос1, и так далее...
 
 public class Engine {
-    private static final int ROUNDS = 3;
+    public static final int ROUNDS = 3;
 
-    public static void check(String[] data) {
-        int countCorrectAnswer = 0;
+    public static String greetingUser() {
+        Scanner scannerName = new Scanner((System.in));
 
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+
+        String userName = scannerName.next();
+
+        System.out.println(("Hello, " + userName + "!"));
+
+        return userName;
+    }
+
+    public static void gameEven() {
+        String userName = greetingUser();
+
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+
+        check(Even.returnDataForEven(), userName);
+    }
+
+    public static void gameCalc() {
+        String userName = greetingUser();
+
+        System.out.println("What is the result of the expression?");
+
+        check(Calc.returnDataForCalc(), userName);
+    }
+
+    public static void gameGcd() {
+        String userName = greetingUser();
+
+        System.out.println("Find the greatest common divisor of given numbers.");
+
+        check(Gcd.returnDataForGcd(), userName);
+    }
+
+    public static void gameProgression() {
+        String userName = greetingUser();
+
+        System.out.println("What number is missing in the progression?");
+
+        check(Progression.returnDataForProgression(), userName);
+    }
+
+    public static void gamePrime() {
+        String userName = greetingUser();
+
+        System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+
+        check(Prime.returnDataForPrime(), userName);
+    }
+
+
+    public static void check(String[][] data, String userName) {
         for (int i = 0; i < ROUNDS; i++) {
-            System.out.println("Question: " + data[i]);
+            System.out.println("Question: " + data[i][0]);
             System.out.print("Your answer: ");
 
             Scanner scannerUserAnswer = new Scanner(System.in);
             String userAnswer = scannerUserAnswer.next();
 
-            if (userAnswer.equals(data[i + ROUNDS])) {
+            if (userAnswer.equals(data[i][1])) {
                 System.out.println("Correct");
-                countCorrectAnswer += 1;
-
-                if (countCorrectAnswer == ROUNDS) {
-                    System.out.println("Congratulations, " + Util.getUserName() + "!");
-                }
             } else {
                 System.out.print("'" + userAnswer + "' " + "is wrong answer ;(.");
-                System.out.println(" Correct answer was " + "'" + data[i + ROUNDS] + "'.");
+                System.out.println(" Correct answer was " + "'" + data[i][1] + "'.");
 
-                System.out.println("Let's try again, " + Util.getUserName() + "!");
-                System.exit(0);
+                System.out.println("Let's try again, " + userName + "!");
+                return;
             }
         }
+        System.out.println("Congratulations, " + userName + "!");
     }
 }
