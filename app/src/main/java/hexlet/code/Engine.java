b8a9fdfa5316eb767b08a3ae02bbfbc5b1495f1e
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2025-03-29 14:50:01 +0300

    correcting the logic of Engine

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,11 +1,5 @@
 package hexlet.code;
 
-import hexlet.code.games.Calc;
-import hexlet.code.games.Even;
-import hexlet.code.games.Gcd;
-import hexlet.code.games.Prime;
-import hexlet.code.games.Progression;
-
 import java.util.Scanner;
 
 // Формат двумерного массива data -> [[вопрос1, ответ1], [вопрос2, ответ2], [вопрос3, ответ3]]
@@ -15,67 +9,28 @@ import java.util.Scanner;
 public class Engine {
     public static final int ROUNDS = 3;
 
-    public static String greetingUser() {
+    public static String scanner() {
         Scanner scannerName = new Scanner((System.in));
+        return scannerName.next();
+    }
 
+    public static void invitationToTheGame () {
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
+    }
 
-        String userName = scannerName.next();
-
+    public static void greetingUser(String userName) {
         System.out.println(("Hello, " + userName + "!"));
-
-        return userName;
     }
 
-    public static void gameEven() {
-        String userName = greetingUser();
-
-        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-
-        check(Even.returnDataForEven(), userName);
-    }
-
-    public static void gameCalc() {
-        String userName = greetingUser();
-
-        System.out.println("What is the result of the expression?");
-
-        check(Calc.returnDataForCalc(), userName);
-    }
-
-    public static void gameGcd() {
-        String userName = greetingUser();
-
-        System.out.println("Find the greatest common divisor of given numbers.");
-
-        check(Gcd.returnDataForGcd(), userName);
-    }
-
-    public static void gameProgression() {
-        String userName = greetingUser();
-
-        System.out.println("What number is missing in the progression?");
-
-        check(Progression.returnDataForProgression(), userName);
-    }
-
-    public static void gamePrime() {
-        String userName = greetingUser();
-
-        System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
-
-        check(Prime.returnDataForPrime(), userName);
-    }
-
-
+    // Возможно, метод следует тоже разделить на контексты.
+    // Или этого в целом достаточно??
     public static void check(String[][] data, String userName) {
         for (int i = 0; i < ROUNDS; i++) {
             System.out.println("Question: " + data[i][0]);
             System.out.print("Your answer: ");
 
-            Scanner scannerUserAnswer = new Scanner(System.in);
-            String userAnswer = scannerUserAnswer.next();
+            String userAnswer = scanner();
 
             if (userAnswer.equals(data[i][1])) {
                 System.out.println("Correct");
@@ -88,5 +43,17 @@ public class Engine {
             }
         }
         System.out.println("Congratulations, " + userName + "!");
+    }
+
+    public static void gameStart(String[][] data, String rule) {
+        invitationToTheGame(); // Приглашение к игре (грубо говоря заставка перед взаимодействием с user)
+
+        String userName = scanner(); // Получаю имя пользователя
+
+        greetingUser(userName); // Приветствую пользователя
+
+        System.out.println(rule); // Показываю правило
+
+        check(data, userName); // Задаю вопрос, проверяю ответ
     }
 }
