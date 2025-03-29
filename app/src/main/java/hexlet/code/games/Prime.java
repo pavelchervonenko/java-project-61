f2e6d042d3e7cd6bca6bc10192f9da5493b1bba7
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2025-03-29 14:51:46 +0300

    made the code cleaner

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -7,6 +7,17 @@ public class Prime {
     private static final int MAX_NUMBER = 150; // Максимальное число для генерации
     private static final int PARE = 2;
 
+    public static boolean isPrime(int numberInteger) {
+        int flag = 0;
+
+        for (int j = 2; j * j <= numberInteger; j++) {
+            if (numberInteger % j == 0) {
+                flag = 1;
+                break;
+            }
+        }
+        return flag == 0;
+    }
     public static String[][] returnDataForPrime() {
         String[][] data = new String[Engine.ROUNDS][PARE];
         int numberInteger;
@@ -17,23 +28,16 @@ public class Prime {
             String numberString = String.valueOf(numberInteger);
             data[i][0] = numberString;
 
-            int flag = 0;
-            for (int j = 2; j * j <= numberInteger; j++) {
-                if (numberInteger % j == 0) {
-                    data[i][1] = "no";
-                    flag = 1;
-                    break;
-                }
-            }
-            if (flag == 0) {
-                data[i][1] = "yes";
-            }
+            data[i][1] = isPrime(numberInteger) ? "yes" : "no";
 
         }
         return data;
     }
 
     public static void gamePrime() {
-        Engine.gamePrime();
+        String[][] gameData = returnDataForPrime();
+        String rule = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+
+        Engine.gameStart(gameData, rule);
     }
 }
