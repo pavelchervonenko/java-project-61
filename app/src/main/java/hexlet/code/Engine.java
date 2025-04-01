fae492f52232d75664dad69706238fb7204c88a0
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2025-04-02 00:39:45 +0300

    combined it into one method

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -9,51 +9,32 @@ import java.util.Scanner;
 public class Engine {
     public static final int ROUNDS = 3;
 
-    public static String scanner() {
-        Scanner scannerName = new Scanner((System.in));
-        return scannerName.next();
-    }
-
-    public static void invitationToTheGame() {
+    public static void gameStart(String[][] data, String rule) {
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
-    }
 
-    public static void greetingUser(String userName) {
+        Scanner scanner = new Scanner((System.in));
+        String userName = scanner.next();
+
         System.out.println(("Hello, " + userName + "!"));
-    }
 
-    // Возможно, метод следует тоже разделить на контексты.
-    // Или этого в целом достаточно??
-    public static void check(String[][] data, String userName) {
+        System.out.println(rule);
+
         for (int i = 0; i < ROUNDS; i++) {
             System.out.println("Question: " + data[i][0]);
             System.out.print("Your answer: ");
 
-            String userAnswer = scanner();
+            String userAnswer = scanner.next();
 
             if (userAnswer.equals(data[i][1])) {
                 System.out.println("Correct");
             } else {
                 System.out.print("'" + userAnswer + "' " + "is wrong answer ;(.");
                 System.out.println(" Correct answer was " + "'" + data[i][1] + "'.");
-
                 System.out.println("Let's try again, " + userName + "!");
                 return;
             }
         }
         System.out.println("Congratulations, " + userName + "!");
-    }
-
-    public static void gameStart(String[][] data, String rule) {
-        invitationToTheGame(); // Приглашение к игре (грубо говоря заставка перед взаимодействием с user)
-
-        String userName = scanner(); // Получаю имя пользователя
-
-        greetingUser(userName); // Приветствую пользователя
-
-        System.out.println(rule); // Показываю правило
-
-        check(data, userName); // Задаю вопрос, проверяю ответ
     }
 }
